de89b169fef16b1919c638fec395762f9778bdac
kingkrazee <kaydenhendrick04@gmail.com>
2024-06-12 16:27:10 +0200

    pros

--- script/cart.js
+++ script/cart.js
@@ -1,10 +1,11 @@
 let items = JSON.parse(localStorage.getItem('items'));
-let purchased = JSON.parse(localStorage.getItem('cart'));
+let purchased = JSON.parse(localStorage.getItem('cart'))||[];
 let main = document.getElementById('thecart')
 let btn = document.querySelector('button')
     purchased.forEach(item => {
         main.innerHTML += `<tr>
-                    <td>${item.image}</td>
+                    <td><img src="${item.image}" alt="${item.name}"></td>
+                    <td${item.name}<td/>
                     <td>${item.price}</td>
                     </tr>
                             `;
--- script/products.js
+++ script/products.js
@@ -17,22 +17,43 @@
  const item6 = new createItem(1, 'Flower Boy','Suits','https://kingkrazee.github.io/eccomerce/images/suit4.webp','I can buy myself flowers', 1, 1500.00);
  let items = [item1, item2, item3, item4, item5, item6]
  let main = document.getElementById('boughting');
- localStorage.setItem('items',JSON.stringify('items'));
- items.forEach(item => {
-    main.innerHTML += `
-    <div>
-    <img src="${item.image}">
-    <p>R${item.price}</p>
-    <button id="View">View More</button>
-    <button class"buy" value=${item.id}">Purchase</button>
-    </d>`
- });
- let buyItems = []
+ let itemsHTML = items.map(item => `
+    <div class="col">
+        <div class="card h-100">
+            <img src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.category}</h5>
+                <p class="card-text">${item.description}</p>
+                <p class="card-text">Price: R${item.price}</p>
+                <button class="buy btn btn-primary" value="${item.id}">Purchase</button>
+            </div>
+        </div>
+    </div>
+`).join('');
+
+// Construct the row and container HTML
+let containerHTML = `
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            ${itemsHTML}
+        </div>
+    </div>
+`;
+
+// Append the container HTML to the main element
+main.innerHTML = containerHTML;
+ let buyItems = JSON.parse(localStorage.getItem('cart'))||[];
  let buyBtns = document.querySelectorAll('.buy');
  function addTocart(id){
     let [item] = items.filter(object => object.id === +id)
-    buyItems.push(item);
-    localStorage.setItem('cart', JSON.stringify(buyItems));
+    let existingItemIndex = buyItems.findIndex(item => item.id === itemToAdd.id);
+    if (existingItemIndex !== -1){
+        buyItems[existingItemIndex].quantity++;
+    }else {
+        itemToAdd.quantity = 1;
+        buyItems.push(itemToAdd);
+    }
+    localStorage.setItem('cart', JSON.stringify(buyItems))
 
  }
  buyBtns.forEach(button =>{
@@ -40,6 +61,3 @@
         addTocart(event.target.value);
     })
  })
-
- displayItems(items);
-
